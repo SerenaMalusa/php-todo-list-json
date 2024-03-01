37c127b42466a2ceeced09122f577c632037f5f7
SerenaMalusa <sere.malu@gmail.com>
2024-03-01 16:06:14 +0100

    feat: error handling

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -5,6 +5,8 @@ const app = createApp ({
         return {
             tasks: [],
             newTask: '',
+            isInputEmpty: false,
+            message: '',
             endpoint: '../backend/api/',
             // endpoint: 'http://localhost/php-todo-list-json/backend/api/',
         }
@@ -21,21 +23,50 @@ const app = createApp ({
         },
         // function that adds a new task to the db
         addTask() {
-            // save the new task text in the data parameter
-            const data =  {
-                newTask: this.newTask,
-            };
-            // axios call POST
-            axios.post( this.endpoint + 'add-task.php', data, 
-            {
-                headers: { 'Content-Type': 'multipart/form-data' }
-            }).then((res) => {
-                // then prints the response in the tasks array
-                this.tasks = res.data;
-            })
-            // empty the input 
-            this.newTask = '';
 
+            if(!this.newTask) {
+
+                // set the switch variable to true
+                this.isInputEmpty = true;
+                // show the tag for the "error" message
+                this.message = 'error';
+                // cancel the message and reset input style after 30sec
+                this.cancelMessage();
+                // stop everything
+                return;
+
+            } else {
+                
+                // set the switch variable to false
+                this.isInputEmpty = false;
+                // save the new task text in the data parameter
+                const data =  {
+                    newTask: this.newTask,
+                };
+                // axios call POST
+                axios.post( this.endpoint + 'add-task.php', data, 
+                {
+                    headers: { 'Content-Type': 'multipart/form-data' }
+                }).then((res) => {
+                    // then prints the response in the tasks array
+                    this.tasks = res.data;
+                })
+                // empty the input 
+                this.newTask = '';
+                // show the tag for the "done" message
+                this.message = 'done';
+                // cancel the message after 30sec
+                this.cancelMessage();
+
+            }
+
+        },
+        // cancel the message and reset input style after 15sec
+        cancelMessage() {
+            setTimeout(() => {
+                this.isInputEmpty = false;
+                this.message = '';
+            }, 1000 * 15);
         },
     },
     mounted() {
