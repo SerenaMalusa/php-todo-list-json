cf2138c4abd8f673976b8d858c5d5bdd895fdab4
SerenaMalusa <sere.malu@gmail.com>
2024-03-01 15:54:47 +0100

    feat: add a task with the input

--- backend/api/add-task.php
+++ backend/api/add-task.php
@@ -1,7 +1,23 @@
 <?php
 
-$json_tasks = file_get_contents('../data/tasks.json');
+// get the new task from the POST and save it in an array
+$new_task_text = $_POST['newTask'];
+$new_task = [
+    'text' => $new_task_text,
+    'done' => false
+];
 
+// get the array from the db and render it usable in php
+$json_tasks = file_get_contents('../data/tasks.json');
 $tasks = json_decode($json_tasks, true);
 
-var_dump($tasks);
+// add the new task to the tasks array
+$tasks[] = $new_task;
+
+// transform the final array in json and rewrite the db
+$json_tasks = json_encode($tasks);
+file_put_contents('../data/tasks.json', $json_tasks);
+
+// print the response
+header('Content-Type: application/json');
+echo $json_tasks;
--- frontend/js/main.js
+++ frontend/js/main.js
@@ -5,29 +5,36 @@ const app = createApp ({
         return {
             tasks: [],
             newTask: '',
-            endpoint: '../../backend/api/',
+            endpoint: '../backend/api/',
+            // endpoint: 'http://localhost/php-todo-list-json/backend/api/',
         }
     },
     methods: {
+        // function that gets the taks from the db
         fetchTasks() {
-
+            //axios call to the get-tasks.php file
             axios.get( this.endpoint + 'get-tasks.php' ).then((res) => {
+                // then prints the response in the tasks array
                 this.tasks = res.data;
             });
 
         },
+        // function that adds a new task to the db
         addTask() {
-
+            // save the new task text in the data parameter
             const data =  {
                 newTask: this.newTask,
             };
-
+            // axios call POST
             axios.post( this.endpoint + 'add-task.php', data, 
             {
                 headers: { 'Content-Type': 'multipart/form-data' }
             }).then((res) => {
-                console.log(res);
+                // then prints the response in the tasks array
+                this.tasks = res.data;
             })
+            // empty the input 
+            this.newTask = '';
 
         },
     },
